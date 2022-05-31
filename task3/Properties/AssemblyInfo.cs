26f3989153cde6e966d1354f3f493247afeb88dc
chaoticgoodboi <100448932+fuccenkity@users.noreply.github.com>
2022-06-01 00:24:09 +0300

    bababooey

---
+++ task3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("task3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31d735aa-bbe8-4473-b2bf-e620f13ab9eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ task3/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace task3
+{
+
+
+    /*
+     * Cтворити програму, в якій описати сценарії та покрити функціональними тестами, 
+        блок фільтрації на будь-якому сайті (інтернет магазині) підкатегорії по першому 
+        популярному товару та перевірити, що кількість відфільтрованих товарів дорівнює 
+        цифрі відображеної біля назви імені товару ДО включення фільтра. Оцінити 
+        покриття тестування програми.
+        
+        Покриття потреб - 100%
+        Покриття можливих сценаріїв - 
+        сценарій виставлення максимальної ціни та сценарій виставлення фільтру
+    */
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        private string url= "https:\\elmir.ua\\vacuum_cleaners";
+        private FirefoxDriver driver = new FirefoxDriver("C:\\Program Files\\Mozilla Firefox\\Gecko");
+        WebDriverWait wait;
+        //SETUP: get to the first popular thingy
+        [SetUp]
+        public void SetUp()
+        {
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(9));
+            driver.Navigate().GoToUrl(url);
+            wait.Until<IWebElement>((driver) => driver.FindElement(By.Id("logo")));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+
+        //TEST: 
+
+        //TEST:the quantity of filtered things is exactly like the quantity pointed in the checkbox
+        [Test]
+        public void ChecknTick()
+        {
+            var filter = driver.FindElement(By.XPath("//ul[@class=\"filter-list\"]/li[not(@class=\"more\")]/a[@class=\"filter-value-link\"]"));// /a[@class=\"filter-value-link\"]
+            string tmp = filter.FindElement(By.ClassName("vendor-count")).Text;
+            tmp = tmp.Substring(1, tmp.Length - 2);
+            int expected = int.Parse(tmp);
+            filter.Click();
+            wait.Until<IWebElement>((driver) => driver.FindElement(By.Name("gofilter"))).Click();
+            int reality = driver.FindElement(By.Id("vitrina-tovars")).FindElements(By.TagName("li")).Count; 
+            Assert.AreEqual(expected, reality);
+        }
+
+        [Test]
+        public void MaxPriceCheck()
+        {
+            int maxPrice = int.Parse(driver.FindElement(By.Id("maxPrice")).GetAttribute("placeholder"));
+            driver.Navigate().GoToUrl(driver.Url+"?orderby=cost&orderdir=desc");
+            var a  =driver.FindElement(By.XPath("//*[@id=\"vitrina-tovars\"]/li[1]//*[contains(@class, \"tovar-price\")]/span")).FindElements(By.CssSelector("*:not([style*=\"display: none\"])"));
+            string b = "";
+            foreach(var z in a) { b += z.Text; }
+            int actualmaxprice = int.Parse(b);
+            Assert.IsTrue(maxPrice > actualmaxprice);
+        }
+
+
+        //TEST: 
+    }
+}
